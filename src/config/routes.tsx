c5005315b4ae9bcd24f53adce3760cef626d14de
doria76593 <441419849@qq.com>
2023-11-24 13:51:18 +0800

    删除无用代码 改造路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,4 @@
 import { RouteRecordRaw } from 'vue-router';
-import { About } from '../views/About';
-import { Home } from '../views/Home';
 import { Welcome } from '../views/Welcome';
 import { Fourth } from '../components/welcome/Fourth';
 import { First } from '../components/welcome/First';
@@ -8,8 +6,7 @@ import { Second } from '../components/welcome/Second';
 import { Third } from '../components/welcome/Third';
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  { path: '/', redirect: '/welcome' },
   {
     path: '/welcome',
     component: Welcome,
@@ -18,7 +15,7 @@ export const routes: RouteRecordRaw[] = [
       { path: '2', component: Second },
       { path: '3', component: Third },
       { path: '4', component: Fourth },
-      { path: '', component: First },
+      { path: '', redirect: '/welcome/1' },
     ],
   },
 ];
