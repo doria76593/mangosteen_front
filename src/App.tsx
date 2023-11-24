944727a8822b6c17ee11ba680c45f2af70057851
doria76593 <441419849@qq.com>
2023-11-24 10:17:15 +0800

    删去无用代码

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,8 @@ import { defineComponent, ref } from 'vue';
 
 export const App = defineComponent({
   setup() {
-    const count = ref(0);
-    const onClick = () => {
-      count.value += 100;
-    };
     return () => (
       <div>
-        <h2>{count.value}</h2>
-        <button onClick={onClick}>+100</button>
-        <br />
-        <router-link to="/">Go to Home</router-link>
-        <router-link to="/about">Go to About</router-link>
         <router-view></router-view>
       </div>
     );
