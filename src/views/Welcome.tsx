6e45fd2edba3cf40372be1986e979ed3ea5ffe37
doria76593 <441419849@qq.com>
2023-11-27 11:59:39 +0800

    添加useSwipe

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,30 +1,34 @@
-import { Transition, VNode, defineComponent } from 'vue';
+import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
 import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { useSwipe } from '../hooks/useSwipe';
 import s from './Welcome.module.scss';
-import logo from '../assets/icons/mangosteen.svg';
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement | null>(null);
+    const { direction, swiping } = useSwipe(main);
+    watchEffect(() => {
+      console.log(swiping.value, direction.value);
+    });
     return () => (
       <div class={s.wrapper}>
         <header>
-          <img src={logo} />
+          <svg>
+            <use xlinkHref="#mangosteen"></use>
+          </svg>
           <h1>山竹记账</h1>
         </header>
-        <main class={s.main}>
+        <main class={s.main} ref={main}>
           <RouterView name="main">
-            {({ Component: X, route: R }: { Component: VNode; route: RouteLocationNormalizedLoaded }) => {
-              // console.log('obj', obj);
-              return (
-                <Transition
-                  enterFromClass={s.slide_fade_enter_from}
-                  enterActiveClass={s.slide_fade_enter_active}
-                  leaveToClass={s.slide_fade_leave_to}
-                  leaveActiveClass={s.slide_fade_leave_active}
-                >
-                  {X}
-                </Transition>
-              );
-            }}
+            {({ Component: X, route: R }: { Component: VNode; route: RouteLocationNormalizedLoaded }) => (
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {X}
+              </Transition>
+            )}
           </RouterView>
         </main>
         <footer>
