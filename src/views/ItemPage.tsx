30aa62ca60c56992090917f637be02c544436cfb
doria76593 <441419849@qq.com>
2023-11-28 11:07:01 +0800

    /items/create 页面

--- src/views/ItemPage.tsx
+++ src/views/ItemPage.tsx
@@ -1,5 +1,7 @@
 import { defineComponent, PropType } from 'vue';
 import s from './ItemPage.module.scss';
+import { MainLayout } from '../layouts/MainLayout';
+import { RouterView } from 'vue-router';
 export const ItemPage = defineComponent({
   props: {
     name: {
@@ -7,6 +9,6 @@ export const ItemPage = defineComponent({
     },
   },
   setup: (props, context) => {
-    return () => <div class={s.wrapper}>hi</div>;
+    return () => <RouterView />;
   },
 });
