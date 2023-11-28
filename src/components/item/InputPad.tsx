88bdbfe147200c58ff88545a111e0ceec5487812
doria76593 <441419849@qq.com>
2023-11-28 15:16:45 +0800

    inputpad第二次布局

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,8 +1,8 @@
 import { defineComponent, PropType, ref } from 'vue';
 import { Icon } from '../../shared/Icon';
-import s from './InputPad.module.scss';
 import { time } from '../../shared/time';
-import { Calendar } from 'vant';
+import s from './InputPad.module.scss';
+import { DatePicker, NumberKeyboard, Popup, Calendar } from 'vant';
 export const InputPad = defineComponent({
   props: {
     name: {
@@ -12,44 +12,97 @@ export const InputPad = defineComponent({
   setup: (props, context) => {
     const now = new Date();
     const refDate = ref<Date>(now);
+    const appendText = (n: number | string) => (refAmount.value += n.toString());
+    const buttons = [
+      {
+        text: '1',
+        onClick: () => {
+          appendText(1);
+        },
+      },
+      {
+        text: '2',
+        onClick: () => {
+          appendText(2);
+        },
+      },
+      {
+        text: '3',
+        onClick: () => {
+          appendText(3);
+        },
+      },
+      {
+        text: '4',
+        onClick: () => {
+          appendText(4);
+        },
+      },
+      {
+        text: '5',
+        onClick: () => {
+          appendText(5);
+        },
+      },
+      {
+        text: '6',
+        onClick: () => {
+          appendText(6);
+        },
+      },
+      {
+        text: '7',
+        onClick: () => {
+          appendText(7);
+        },
+      },
+      {
+        text: '8',
+        onClick: () => {
+          appendText(8);
+        },
+      },
+      {
+        text: '9',
+        onClick: () => {
+          appendText(9);
+        },
+      },
+      {
+        text: '.',
+        onClick: () => {
+          appendText('.');
+        },
+      },
+      {
+        text: '0',
+        onClick: () => {
+          appendText(0);
+        },
+      },
+      { text: '清空', onClick: () => {} },
+      { text: '提交', onClick: () => {} },
+    ];
     const refDatePickerVisible = ref(false);
+    const showDatePicker = () => (refDatePickerVisible.value = true);
+
     const onConfirm = (value: any) => {
       refDatePickerVisible.value = false;
       refDate.value = new Date(value);
     };
-    const buttons = [
-      { text: '1', onClick: () => {} },
-      { text: '2', onClick: () => {} },
-      { text: '3', onClick: () => {} },
-      { text: '清空', onClick: () => {} },
-      { text: '4', onClick: () => {} },
-      { text: '5', onClick: () => {} },
-      { text: '6', onClick: () => {} },
-      { text: '+', onClick: () => {} },
-      { text: '7', onClick: () => {} },
-      { text: '8', onClick: () => {} },
-      { text: '9', onClick: () => {} },
-      { text: '-', onClick: () => {} },
-      { text: '.', onClick: () => {} },
-      { text: '0', onClick: () => {} },
-      { text: '删', onClick: () => {} },
-      { text: '提交', onClick: () => {} },
-    ];
+    const refAmount = ref('');
     return () => (
       <>
-        <h2>refShowPop: {refDatePickerVisible.value}</h2>
-
         <div class={s.dateAndAmount}>
           <span class={s.date}>
             <Icon name="date" class={s.icon} />
             <span>
-              <span onClick={() => (refDatePickerVisible.value = true)}>{time(refDate.value).format()}</span>
+              <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
               <Calendar v-model:show={refDatePickerVisible.value} onConfirm={onConfirm} />
             </span>
           </span>
-          <span class={s.amount}>199.12</span>
+          <span class={s.amount}>{refAmount.value}</span>
         </div>
-
         <div class={s.buttons}>
           {buttons.map((button) => (
             <button onClick={button.onClick}>{button.text}</button>
