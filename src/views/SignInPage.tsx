3779b93de6b791936af4bf755b0c69f65daec32b
doria76593 <441419849@qq.com>
2023-12-04 14:32:12 +0800

    mock tag

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -37,7 +37,13 @@ export const SignInPage = defineComponent({
         ])
       );
       if (!hasError(errors)) {
-        const response = await http.post<{ jwt: string }>('/session', formData);
+        const response = await http.post<{ jwt: string }>('/session', formData).catch(onError);
+        // const response = await http
+        //   .post<{ jwt: string }>('/session', formData, {
+        //     params: { _mock: 'session' },
+        //   })
+        //   .catch(onError);
+        console.log(response);
         localStorage.setItem('jwt', response.data.jwt);
         // router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
         // const returnTo = localStorage.getItem('return_to');
